31ff21760912fb20ef90b43702f9c97a89dd33c3
yimingsun12138 <yiming.sun12138@gmail.com>
2023-03-04 13:47:57 +0000

    finish authentication

--- Authentication.R
+++ Authentication.R
@@ -1,6 +1,68 @@
 #' This function is used to set openAI API key.
 #' @param key Your OpenAI API key.
 #' @param organization Usage from the API requests will count against the specified organization's subscription quota. Set to null as default.
+#' @param return_list Whether return the model list?
 
-Auth_OpenAI <- function(key,organization = null){
+Auth_OpenAI <- function(key,organization = NULL,return_list = FALSE){
+  
+  #load required package
+  require(httr)
+  
+  #get model list
+  if(is.null(organization)){
+    model_list <- httr::GET(
+      url = URL_models,
+      httr::add_headers(`Authorization` = paste('Bearer',key,sep = ' '))
+    )
+  }else{
+    model_list <- httr::GET(
+      url = URL_models,
+      httr::add_headers(`Authorization` = paste('Bearer',key,sep = ' ')),
+      httr::add_headers(`OpenAI-Organization` = organization)
+    )
+  }
+  model_list <- httr::content(model_list)
+  
+  #check accessibility of API_key and organization
+  if(is.null(model_list$error)){
+    if(return_list){
+      return(model_list)
+    }else{
+      OpenAI_API_key <<- key
+      if(!is.null(organization)){
+        OpenAI_organization <<- organization
+      }
+      message('All settled!')
+    }
+  }else{
+    stop(model_list$error$message)
+  }
+}
+
+#' Get OpenAI available model list.
+#' @param simplify Whether simplify the list returned?
+OpenAI_model_list <- function(simplify = TRUE){
+  
+  #check accessibility of API_key
+  if(!exists(x = 'OpenAI_API_key')){
+    stop('First verify your OpenAI API key using Auth_OpenAI!')
+  }
+  
+  #get model list
+  if(is.null(OpenAI_organization)){
+    model_list <- Auth_OpenAI(key = OpenAI_API_key,organization = OpenAI_organization,return_list = TRUE)
+  }else{
+    model_list <- Auth_OpenAI(key = OpenAI_API_key,organization = NULL,return_list = TRUE)
+  }
+  
+  #simplify
+  if(simplify){
+    temp <- model_list$data
+    temp <- base::unlist(x = base::lapply(X = temp,FUN = function(x){
+      return(x$id)
+    }))
+    return(temp)
+  }else{
+    return(model_list)
+  }
 }
